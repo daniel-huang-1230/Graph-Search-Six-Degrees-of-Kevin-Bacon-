0950ac83fb31a9dd5db9ee39b06cdf5b8dad0834
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-02-28 10:23:12 -0800

    first commit

--- ActorGraph.cpp
+++ ActorGraph.cpp
@@ -1,7 +1,7 @@
 /*
  * ActorGraph.cpp
- * Author: <YOUR NAME HERE>
- * Date:   <DATE HERE>
+ * Author: Daniel Huang
+ * Date:   2/28/2017
  *
  * This file is meant to exist as a container for starter code that you can use to read the input file format
  * defined in movie_casts.tsv. Feel free to modify any/all aspects as you wish.
